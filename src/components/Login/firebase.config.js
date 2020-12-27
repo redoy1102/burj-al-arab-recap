5374f1f59fc79b2add8ab65f3a5f83d529b2a6cf
Md Fuadul Islam Redoy <mdfuadulislamhridoy52@gmail.com>
2020-12-27 15:31:44 +0600

    firebase config issues solved

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBd_ZvI3pLNIw9ieB5D_moFfcR4JGcPAgQ",
+    authDomain: "burj-al-arab-recap.firebaseapp.com",
+    projectId: "burj-al-arab-recap",
+    storageBucket: "burj-al-arab-recap.appspot.com",
+    messagingSenderId: "326017050057",
+    appId: "1:326017050057:web:f4b231751800ffe8cb3418"
+};
