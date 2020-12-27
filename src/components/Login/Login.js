7913d62464f96bdddd350bae94876477bf07312a
Md Fuadul Islam Redoy <mdfuadulislamhridoy52@gmail.com>
2020-12-27 17:43:06 +0600

    Private Route Done

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,9 +3,14 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import firebaseConfig from "./firebase.config";
 import {UserContext} from "../../App";
+import {useHistory, useLocation} from 'react-router-dom'
 
 const Login = () => {
     const [loggedInUser, setLoggedInUser] = useContext(UserContext)
+    const history = useHistory();
+    const location = useLocation();
+    const { from } = location.state || { from: { pathname: "/" } };
+
 
     //fixing start duplicate app
     if(firebase.apps.length === 0){
@@ -24,6 +29,7 @@ const Login = () => {
             const {displayName, email} = result.user;
             const signedInUser = {name: displayName, email};
             setLoggedInUser(signedInUser)
+            history.replace(from);
 
         }).catch(function (error) {
             // Handle Errors here.
